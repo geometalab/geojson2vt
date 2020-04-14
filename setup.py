622c7cc7f2e49c686ab6f5cde431cd3f5f00e3af
murthy <samuel.kurath@gmail.com>
2020-04-14 15:39:42 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from io import open
+from setuptools import setup, find_packages
+
+with open('README.rst', 'rb') as f:
+    readme = f.read().decode('utf-8')
+
+setup(
+    name='geojson2vt',
+    version='0.0.1',
+    description='Python package to convert GeoJSON to vector tiles',
+    long_description=readme,
+    author='Samuel Kurath',
+    author_email='geometalab@hsr.ch',
+    url='https://github.com/geometalab/geojson2vt',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
