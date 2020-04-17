6237b19671ed96071a3b55e0cb78f3f43ecaa2f7
murthy <samuel.kurath@gmail.com>
2020-04-17 07:38:48 +0200

    updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', 'rb') as f:
 
 setup(
     name='geojson2vt',
-    version='1.0.0',
+    version='1.0.1',
     description='Python package to convert GeoJSON to vector tiles',
     long_description=readme,
     author='Samuel Kurath',
